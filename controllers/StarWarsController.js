77dbfd72ff052eec82d3c852f5863d9042d94d6a
triest <you@example.com>
2020-04-25 18:28:22 +0300

    parse data

--- controllers/StarWarsController.js
+++ controllers/StarWarsController.js
@@ -1,14 +1,11 @@
-
-const getPlanets=async ()=>{
+const getPlanets = async () => {
     let url = require("../config/config.json").starWarsUrl;
     try {
         const axios = require('axios');
         const response = await axios.get(url);
-
-        var   PlanetsResults = response.data.results;
-        return PlanetsResults;
+        return response.data;
     } catch (error) {
-      //  throw new Error("error");
+        //  throw new Error("error");
         return error;
     }
 }
@@ -22,8 +19,10 @@ exports.index = async function (req, res, next) {
     let respons;
     const https = require("https");
     let data;
-    data=await getPlanets();
+    data = await getPlanets();
 
-    return res.json({"task": "ds22", "url": url, "data": data});
+    data.results.forEach(element=>console.log(element))
+
+    return res.json(data.count);
 
 };
